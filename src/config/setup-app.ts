97785d0427d2e90a9fc50e965dc54719f97f727a
koohpeyma <rezakoohpeyma2@gmail.com>
2026-07-22 18:19:57 +0330

    fix(interceptors): add global promise resolver
    - Implement ResolvePromisesInterceptor and deepResolvePromises utility
    - Await all lazy-loaded data in responses to bypass class-transformer issue #549

--- src/config/setup-app.ts
+++ src/config/setup-app.ts
@@ -3,8 +3,10 @@ import { ConfigService } from "@nestjs/config";
 import { NestFastifyApplication } from "@nestjs/platform-fastify";
 import { useContainer } from "class-validator";
 import { AllConfigType } from "./config.type";
-import { ValidationPipe } from "@nestjs/common";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 import validationOptions from "@/utils/validation-option";
+import { Reflector } from "@nestjs/core";
+import { ResolvePromisesInterceptor } from "@/utils/serializer.interceptor";
 
 export function setupApp(app: NestFastifyApplication) {
 	const configService = app.get(ConfigService<AllConfigType>);
@@ -19,4 +21,11 @@ export function setupApp(app: NestFastifyApplication) {
 		{ exclude: ["/"] },
 	);
 	app.useGlobalPipes(new ValidationPipe(validationOptions));
+	
+	  app.useGlobalInterceptors(
+    // ResolvePromisesInterceptor is used to resolve promises in responses because class-transformer can't do it
+    // https://github.com/typestack/class-transformer/issues/549
+    new ResolvePromisesInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
 }
