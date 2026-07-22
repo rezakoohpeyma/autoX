80e9a829476c4a8433a4916684f50ea388ef461d
koohpeyma <rezakoohpeyma2@gmail.com>
2026-07-22 22:45:38 +0330

    feat(security): implement global rate limiting
    - Configure ThrottlerModule in AppModule
    - Bind ThrottlerGuard globally using APP_GUARD
    - Enable trustProxy in Fastify adapter for correct IP tracking

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { envValidation } from "./config/env.validation";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 @Module({
 	imports: [
 		ConfigModule.forRoot({
@@ -8,6 +10,20 @@ import { envValidation } from "./config/env.validation";
 			cache: true,
 			validationSchema: envValidation,
 		}),
+		 ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
 	],
+	providers : [
+		{
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+	]
 })
 export class AppModule {}
